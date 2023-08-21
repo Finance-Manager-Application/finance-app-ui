5e35b77c1719d186669b12aed764651a8620f40c
Darrel Mendoza <darrelmendoza85@gmail.com>
2023-08-21 22:42:49 +0800

    feat(login-page): created login page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { ExpenseTrackerComponent } from './expense-tracker/expense-tracker.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
-    ExpenseTrackerComponent
+    ExpenseTrackerComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
